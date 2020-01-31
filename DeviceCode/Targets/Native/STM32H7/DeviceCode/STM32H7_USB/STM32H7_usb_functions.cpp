56b4318b2b964e14a3a1f8783836c2a413530a57
Nathan Stohs <nathan.stohs@samraksh.com>
2020-01-31 17:46:01 -0500

    USB CDC: Add TX locking

--- DeviceCode/Targets/Native/STM32H7/DeviceCode/STM32H7_USB/STM32H7_usb_functions.cpp
+++ DeviceCode/Targets/Native/STM32H7/DeviceCode/STM32H7_USB/STM32H7_usb_functions.cpp
@@ -25,6 +25,11 @@ typedef struct {
 //	unsigned RxQueueBytes;
 } usb_cdc_status_t;
 
+typedef enum {
+	usb_lock_none =0,
+	usb_lock_tx	  =1,
+} usb_lock_id_t;
+
 extern USBD_HandleTypeDef hUsbDeviceFS; // in usb_device.c
 extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
@@ -32,6 +37,8 @@ static bool USB_initialized = FALSE;
 static uint8_t rx_pkt_buf[64]; 				// TODO: Assumes packets are handled fast
 static uint8_t tx_pkt_buf[256]; 			// 4 packets
 static usb_cdc_status_t usb_cdc_status;
+
+static uint32_t usb_lock; // For locking TX
 
 // Core USB CDC callbacks
 static int8_t CDC_Init_FS(void);
@@ -53,6 +60,38 @@ USBD_CDC_ItfTypeDef USBD_Interface_fops_FS =
 static inline bool isInterrupt(void) __attribute__ ((unused));
 static inline bool isInterrupt() {
     return (SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk) != 0;
+}
+
+// Returns true if lock aquired
+static bool get_lock_inner(volatile uint32_t *Lock_Variable, usb_lock_id_t id) {
+	uint32_t status;
+	__DSB();
+
+	if (__LDREXW(Lock_Variable) != usb_lock_none) {
+		__CLREX(); // AM I NEEDED?
+		return false;
+	}
+
+	status = __STREXW(id, Lock_Variable);
+	return (status == usb_lock_none);
+}
+
+// Should try more than once, can legit fail even if lock is free.
+// Example, will never succeed if any interrupt hits in between.
+// ldrex-strex only guarantees that lock is free when it says so, but NOT the inverse.
+static uint32_t get_lock(volatile uint32_t *Lock_Variable, usb_lock_id_t id) {
+	int attempts=3;
+	do {
+		if ( get_lock_inner(Lock_Variable, id) )
+			return 0;
+	} while (--attempts);
+	return *Lock_Variable; // return who we think the blocking owner is. NOT GUARANTEED TO BE RIGHT.
+}
+
+static void free_lock(volatile uint32_t *Lock_Variable) {
+	__DMB();
+	*Lock_Variable = usb_lock_none;
+	return;
 }
 
 static int8_t CDC_Init_FS(void) {
@@ -123,45 +162,35 @@ HRESULT CPU_USB_Uninitialize( int Controller ) {
 	return S_OK;
 }
 
-// Special version which allows call from interrupt
-// Doesn't busy wait but limited to 64 bytes (1 USB packest)
-// static int CPU_USB_write_isr_context(const char *buf, int size)  {
-	// int ret;
-	// const int max_sz = sizeof(tx_pkt_buf);
-	// if ( !is_usb_link_up() ) return size;
-	// if ( is_usb_tx_not_empty() ) return 0; // busy, try again later
-	// if (size > max_sz) size = max_sz;
-	// memcpy(tx_pkt_buf, buf, size);
-	// ret = CDC_Transmit_FS( (uint8_t *)buf, size);
-	// if (ret != USBD_OK) { __BKPT(); return -1; } // Some weird error
-	// usb_cdc_status.TxBytes += size;
-	// return size;
-// }
-
 int CPU_USB_write(const char *buf, int size) {
-	int ret;
+	int ret, lock_ret, usb_ret;
 	const int max_sz = sizeof(tx_pkt_buf);
 
 	if (!USB_initialized)      return -1;	// Never init, hard fail
 	if (!is_usb_link_up())     return size;	// Init but no connection we define as "success"
 	if (is_usb_tx_not_empty()) return 0;	// Busy
 
+	lock_ret = get_lock(&usb_lock, usb_lock_tx);
+	if (lock_ret) return 0; // Failed to get lock, busy
+
+	// Check if busy again after lock acquired
+	if (is_usb_tx_not_empty()) { ret = 0; goto out; }
+
 	if (size > max_sz) size = max_sz;
 	memcpy(tx_pkt_buf, buf, size);
 
-	do {
-		if (!is_usb_link_up()) return size;
-		ret = CDC_Transmit_FS(tx_pkt_buf, size);
-	} while(ret == USBD_BUSY); // Keep trying
+	usb_ret = CDC_Transmit_FS(tx_pkt_buf, size);
 
-	if (ret != USBD_OK) { __BKPT(); return -1; }// Some weird error
+	// Oddball error check
+	if (usb_ret != USBD_OK) { ret = -1; goto out; }
 
-	// Adjust accounting
-	__disable_irq();
+	// Adjust accounting, we are locked so IRQ safe
+	ret = size;
 	usb_cdc_status.TxBytes += size;
-	__enable_irq();
 
-	return size;
+out:
+	free_lock(&usb_lock);
+	return ret;
 }
 
 static int CPU_USB_Queue_Rx_Data( char c ){
