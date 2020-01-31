94ed7cfb255f44cee06815a1845327657fe7fd83
Nathan Stohs <nathan.stohs@samraksh.com>
2020-01-31 16:25:52 -0500

    USB CDC revision
    More careful with the async behavior

--- DeviceCode/Targets/Native/STM32H7/DeviceCode/STM32H7_USB/STM32H7_usb_functions.cpp
+++ DeviceCode/Targets/Native/STM32H7/DeviceCode/STM32H7_USB/STM32H7_usb_functions.cpp
@@ -11,27 +11,86 @@ NPS 2019-11-22
 
 #include <tinyhal.h>
 #include "usb_device.h"
+#include "usbd_cdc_if.h"
 
-#ifndef STRING_BUF_SIZE
-#define STRING_BUF_SIZE 128
-#endif
 
-// Note buffers are large to accomodate testing
+//#define INBUF_IDX usb_cdc_status.RxQueueBytes // Alias, to confuse people later
 
-#ifndef USB_IN_BUF_SIZE
-#define USB_IN_BUF_SIZE (256)
-#endif
+// Singleton status structure
+typedef struct {
+	volatile bool is_connected;
+//	int usb_cdc_overrun;
+	unsigned TxBytes;
+	unsigned RxBytes;
+//	unsigned RxQueueBytes;
+} usb_cdc_status_t;
 
-#define INBUF_IDX usb_cdc_status.RxQueueBytes // Alias, to confuse people later
+extern USBD_HandleTypeDef hUsbDeviceFS; // in usb_device.c
+extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
 static bool USB_initialized = FALSE;
-
-static uint8_t inbuf[USB_IN_BUF_SIZE];		// USB buffer
-											// Using libc for outbuf
-static uint8_t rx_pkt_buf[STRING_BUF_SIZE]; // Max FS packet size is 64 bytes
-static uint8_t tx_pkt_buf[STRING_BUF_SIZE]; // Max FS packet size is 64 bytes
-
+static uint8_t rx_pkt_buf[64]; 				// TODO: Assumes packets are handled fast
+static uint8_t tx_pkt_buf[256]; 			// 4 packets
 static usb_cdc_status_t usb_cdc_status;
+
+// Core USB CDC callbacks
+static int8_t CDC_Init_FS(void);
+static int8_t CDC_DeInit_FS(void);
+static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length);
+static int8_t CDC_Receive_FS(uint8_t* pbuf, uint32_t *Len);
+
+static int CPU_USB_Queue_Rx_Data(char c);
+
+USBD_CDC_ItfTypeDef USBD_Interface_fops_FS =
+{
+	CDC_Init_FS,
+	CDC_DeInit_FS,
+	CDC_Control_FS,
+	CDC_Receive_FS
+};
+
+// Test if in interrupt context
+static inline bool isInterrupt(void) __attribute__ ((unused));
+static inline bool isInterrupt() {
+    return (SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk) != 0;
+}
+
+static int8_t CDC_Init_FS(void) {
+	usb_cdc_status.is_connected = true;
+	USBD_CDC_SetTxBuffer(&hUsbDeviceFS, tx_pkt_buf, 0);
+	USBD_CDC_SetRxBuffer(&hUsbDeviceFS, rx_pkt_buf);
+	return (USBD_OK);
+}
+
+static int8_t CDC_DeInit_FS(void) {
+	usb_cdc_status.is_connected = false;
+	return (USBD_OK);
+}
+
+static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length) {
+	return (USBD_OK);
+}
+
+static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len) {
+	for (int i = 0; i < *Len; i++) {
+	  CPU_USB_Queue_Rx_Data( (char)Buf[i]); // TODO: Can input more than 1 byte at a time
+	}
+	usb_cdc_status.RxBytes += *Len;
+	USBD_CDC_SetRxBuffer(&hUsbDeviceFS, rx_pkt_buf);
+	USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+	return (USBD_OK);
+}
+
+uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len) {
+  uint8_t result = USBD_OK;
+  USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
+  if (hcdc->TxState != 0){
+    return USBD_BUSY;
+  }
+  USBD_CDC_SetTxBuffer(&hUsbDeviceFS, Buf, Len);
+  result = USBD_CDC_TransmitPacket(&hUsbDeviceFS);
+  return result;
+}
 
 static void reset_status(usb_cdc_status_t *x) {
 	memset(x, 0, sizeof(usb_cdc_status_t)); // A simple zero'ing
@@ -41,74 +100,77 @@ static int is_usb_link_up(void) {
 	return usb_cdc_status.is_connected;
 }
 
-extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
-
-extern "C" void OTG_FS_IRQHandler(void)
-{
-  HAL_PCD_IRQHandler(&hpcd_USB_OTG_FS);
+// On going operation
+static int is_usb_tx_not_empty(void) {
+	USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
+	if (hcdc->TxState != 0) return 1;
+	else return 0; // empty
 }
 
-extern "C" uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len); // for debugging
-
-
-HRESULT CPU_USB_Initialize( int Controller )
-{
+HRESULT CPU_USB_Initialize( int Controller ) {
 	if (USB_initialized == false){
 		MX_USB_DEVICE_Init();
+		// Allows USB to work in WFI
+		__HAL_RCC_USB2_OTG_FS_CLK_SLEEP_ENABLE();
+		__HAL_RCC_USB2_OTG_FS_ULPI_CLK_SLEEP_DISABLE();
 		USB_initialized = true;
 	}
 	return S_OK;
 }
 
-HRESULT CPU_USB_Uninitialize( int Controller )
-{
-	USB_initialized = false;
+// No-op
+HRESULT CPU_USB_Uninitialize( int Controller ) {
 	return S_OK;
 }
 
-// Kind of sloppy to be locked for most of it, but it is clean and not worth further investment
-/*int CPU_USB_read(void *buf, int size) {
-	int ret=0;
-	// Don't care about CDC not connected case, simply means nothing to read
-	if (INBUF_IDX == 0) return ret;
-	__disable_irq();
-	if (size >= INBUF_IDX) { // Return the whole input buffer
-		memcpy(buf, inbuf, INBUF_IDX);
-		ret = INBUF_IDX;
-		INBUF_IDX = 0;
-	}
-	else { // Return a piece of the input buffer, shift reminder down
-		memcpy(buf, inbuf, size);
-		memcpy(inbuf, &inbuf[size], sizeof(inbuf)-size); // Does not actually overlap so no need memmove()
-		INBUF_IDX -= size;
-		ret = size;
-	}
-	usb_cdc_status.RxBytes += ret;
-	__enable_irq();
-	return ret;
-}
-*/
+// Special version which allows call from interrupt
+// Doesn't busy wait but limited to 64 bytes (1 USB packest)
+// static int CPU_USB_write_isr_context(const char *buf, int size)  {
+	// int ret;
+	// const int max_sz = sizeof(tx_pkt_buf);
+	// if ( !is_usb_link_up() ) return size;
+	// if ( is_usb_tx_not_empty() ) return 0; // busy, try again later
+	// if (size > max_sz) size = max_sz;
+	// memcpy(tx_pkt_buf, buf, size);
+	// ret = CDC_Transmit_FS( (uint8_t *)buf, size);
+	// if (ret != USBD_OK) { __BKPT(); return -1; } // Some weird error
+	// usb_cdc_status.TxBytes += size;
+	// return size;
+// }
+
 int CPU_USB_write(const char *buf, int size) {
 	int ret;
-	if (!USB_initialized) return 0; // CDC not connected
+	const int max_sz = sizeof(tx_pkt_buf);
+
+	if (!USB_initialized)      return -1;	// Never init, hard fail
+	if (!is_usb_link_up())     return size;	// Init but no connection we define as "success"
+	if (is_usb_tx_not_empty()) return 0;	// Busy
+
+	if (size > max_sz) size = max_sz;
 	memcpy(tx_pkt_buf, buf, size);
-	//do { // TODO: ADD TIMEOUT
+
+	do {
+		if (!is_usb_link_up()) return size;
 		ret = CDC_Transmit_FS(tx_pkt_buf, size);
-	if (ret == USBD_BUSY)
-		return 0;
-	//} while(ret == USBD_BUSY);
-	if (ret != USBD_OK) __BKPT();
+	} while(ret == USBD_BUSY); // Keep trying
+
+	if (ret != USBD_OK) { __BKPT(); return -1; }// Some weird error
+
+	// Adjust accounting
 	__disable_irq();
 	usb_cdc_status.TxBytes += size;
 	__enable_irq();
+
 	return size;
 }
 
-// This function is called from the usb driver c code
-extern "C" int CPU_USB_Queue_Rx_Data(  char c );
-int CPU_USB_Queue_Rx_Data(  char c ){
+static int CPU_USB_Queue_Rx_Data( char c ){
 	// sending back only port number (USB_SERIAL_PORT also contains info that it is a serial interface)
 	return USART_AddCharToRxBuffer(ConvertCOM_ComPort(USB_SERIAL_PORT), c);
+}
+
+extern "C" void OTG_FS_IRQHandler(void) {
+  HAL_PCD_IRQHandler(&hpcd_USB_OTG_FS);
 }
 
 extern "C" void USB_Error_Handler(void);
