074cbd17c3f26c9482b7b6c57b02b0c59835cef8
Nathan Stohs <nathan.stohs@samraksh.com>
2020-03-04 16:52:48 -0500

    Script update

--- update_tinyclr_axf.sh
+++ update_tinyclr_axf.sh
@@ -4,9 +4,10 @@
 # Must be very sure that your binary matches the current tree
 # TODO: Hook into build so that you can't screw this up by invoking at the wrong time
 
+AXF_FILE=BuildOutput/THUMB2FP/GCC6.3/le/FLASH/debug/STM32H743NUCLEO/bin/tinyclr.axf
 LOG_FILE=binaries/tinyclr.version.txt
 
-cp BuildOutput/THUMB2FP/GCC6.3/le/FLASH/debug/STM32H743NUCLEO/bin/tinyclr.axf binaries/
-echo "BuildOutput/THUMB2FP/GCC6.3/le/FLASH/debug/STM32H743NUCLEO/bin/tinyclr.axf" > $LOG_FILE
-date >> $LOG_FILE
+cp $AXF_FILE binaries/
+date > $LOG_FILE
+arm-none-eabi-size $AXF_FILE >> $LOG_FILE
 git describe --always --abbrev=0 >> $LOG_FILE
