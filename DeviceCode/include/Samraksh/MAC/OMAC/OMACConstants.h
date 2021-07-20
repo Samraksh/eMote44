cf462ed2b9a3ee5960a05566a4a7cc829dd89096
Nathan Stohs <nathan.stohs@samraksh.com>
2021-07-20 18:01:18 -0400

    OSU AIRPORT: Change fast disco period to 1 hour

--- DeviceCode/include/Samraksh/MAC/OMAC/OMACConstants.h
+++ DeviceCode/include/Samraksh/MAC/OMAC/OMACConstants.h
@@ -52,7 +52,7 @@
 #define SLOTRETRYMAXATTEMPT 2
 #define CCA_PERIOD_FRAME_RETRY_MICRO 0 //BK: We need to double check this. Since 2 nodes will be off by this much. A node should CCA at least this much to make sure there was no other transmitter trying to reach the same destination.
 
-#define HIGH_DISCO_PERIOD_IN_SLOTS_CONSTANT 8000 //13334 //This is setting the length of high disco period after start up. Note that the length of disco slot is different than the length of data slots. The ratio is determined by DISCOPERIODINSLOTS. Note that this is a convention and there is no direct relationship limiting the ratio between the data slots and the discovery slots.
+#define HIGH_DISCO_PERIOD_IN_SLOTS_CONSTANT 50000 // ~1 hour. This is setting the length of high disco period after start up. Note that the length of disco slot is different than the length of data slots. The ratio is determined by DISCOPERIODINSLOTS. Note that this is a convention and there is no direct relationship limiting the ratio between the data slots and the discovery slots.
 //#define HIGH_DISCO_PERIOD_IN_SLOTS_CONSTANT 0
 #ifdef PLATFORM_ARM_EmoteDotNow
 #define HIGH_DISCO_PERIOD_ALWAYS_ON 1
